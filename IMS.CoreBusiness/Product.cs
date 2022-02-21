4c6f18adab3a82447532f796f3aca49d4c4ec9ea
SmallPeenwow <bowdencamryn@gmail.com>
2022-02-21 10:00:00 +0200

    Validate Product Price Completed

--- IMS.CoreBusiness/Product.cs
+++ IMS.CoreBusiness/Product.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using IMS.CoreBusiness.Validations;
 
 namespace IMS.CoreBusiness
 {
@@ -18,8 +19,24 @@ namespace IMS.CoreBusiness
         public int Quantity { get; set; }
 
         [Range(0, double.MaxValue, ErrorMessage = "Price must be greater or equal to {0}")]
+        [Product_EnsurePriceIsGreaterThanInventoriesPrice]
         public double Price { get; set; }
 
         public List<ProductInventory>? ProductInventories { get; set; }
+
+        public double TotalInventoryCost()
+        {
+            return this.ProductInventories.Sum(x => x.Inventory?.Price * x.InventoryQuantity ?? 0);
+        }
+
+        public bool ValidatePricing()
+        {
+            // Not validating the product object when the product inventories are not loaded
+            if (ProductInventories == null || ProductInventories.Count <= 0) return true;
+
+            if (this.TotalInventoryCost() > Price) return false;
+
+            return true;
+        }
     }
 }
