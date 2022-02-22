70393bdd3dccd6818a295d48c76a003b2463896d
SmallPeenwow <bowdencamryn@gmail.com>
2022-02-22 23:10:45 +0200

    Produce Products Completed

--- IMS.CoreBusiness/ProductTransaction.cs
+++ IMS.CoreBusiness/ProductTransaction.cs
@@ -7,24 +7,24 @@ using System.Threading.Tasks;
 
 namespace IMS.CoreBusiness
 {
-    public class InventoryTransaction
+    public class ProductTransaction
     {
-        public int InventoryTransactionId { get; set; }
+        public int ProductTransactionId { get; set; }
 
         [Required]
-        public int InventoryId { get; set; }
+        public int ProductId { get; set; }
 
         [Required]
         public int QuantityBefore { get; set; }
 
         // The action taken (purchase or product product)
         [Required]
-        public InventoryTransactionType ActivityType { get; set; }
+        public ProductTransactionType ActivityType { get; set; }
 
         [Required]
         public int QuantityAfter { get; set; }
-        public string? PONumber { get; set; }
         public string? ProductionNumber { get; set; }
+        public string? SalesOrderNumber { get; set; }
 
         public double? UnitPrice { get; set; }
 
@@ -35,6 +35,6 @@ namespace IMS.CoreBusiness
         public string DoneBy { get; set; }
 
         //Navigation Properties
-        public Inventory Inventory { get; set; }
+        public Product Product { get; set; }
     }
 }
---
+++ IMS.Plugins.EFCore/ProductTransactionRepository.cs
@@ -0,0 +1,50 @@
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.PluginInterfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.Plugins.EFCore
+{
+    public class ProductTransactionRepository : IProductTransactionRepository
+    {
+        private readonly IMSContext db;
+        private readonly IProductRepository productRepository;
+
+        public ProductTransactionRepository(IMSContext db, IProductRepository productRepository)
+        {
+            this.db = db;
+            this.productRepository = productRepository;
+        }
+
+        public async Task ProduceAsync(string productionNumber, Product product, int quantity, double price, string doneBy)
+        {
+            var prod = this.productRepository.GetProductByIdAsync(product.ProductId);
+
+            if (prod != null)
+            {
+                foreach(var pi in prod.ProductInventories)
+                {
+                    pi.Inventory.Quantity -= quantity * pi.InventoryQuantity;
+                }
+            }
+
+            this.db.ProductTransactions.Add(new ProductTransaction
+            {
+                ProductionNumber = productionNumber,
+                ProductId = product.ProductId,
+                QuantityBefore = product.Quantity,
+                ActivityType = ProductTransactionType.ProduceProduct,
+                QuantityAfter = product.Quantity + quantity,
+                TransactionDate = DateTime.Now,
+                DoneBy = doneBy,
+                UnitPrice = price
+            });
+
+            await this.db.SaveChangesAsync();
+        }
+    }
+}
